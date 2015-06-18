75c5abd598bc4017c8de0588bd0af74c1854c4de
Mona <monika.pejicic@idbgroup.net>
2015-06-18 13:30:00 +0200

    starting project and splash screen

--- app/src/main/java/com/example/qbrapp/qbr/Splash.java
+++ app/src/main/java/com/example/qbrapp/qbr/Splash.java
@@ -18,7 +18,7 @@ public class Splash extends Activity {
         Thread timerThread = new Thread(){
             public void run(){
                 try{
-                    sleep(4000);
+                    sleep(5000);
                 }catch(InterruptedException e){
                     e.printStackTrace();
                 }finally{
