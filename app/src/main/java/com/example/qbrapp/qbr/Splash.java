1ef84735cb815a05389cd7dccfed3481f589e11a
Mona <monika.pejicic@idbgroup.net>
2015-06-18 13:15:14 +0200

    splash screen working

---
+++ app/src/main/java/com/example/qbrapp/qbr/Splash.java
@@ -0,0 +1,40 @@
+package com.example.qbrapp.qbr;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by monika.pejicic on 17.6.2015..
+ */
+public class Splash extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashh);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(4000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent i = new Intent("com.example.qbrapp.qbr.MAINACTIVITY");
+                    startActivity(i);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+
+}
